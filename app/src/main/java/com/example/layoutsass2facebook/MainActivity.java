b2138f081588bb1d4e6ab7c89c72005bfa45adfa
saurabh09111996 <saurabhsharma708@gmail.com>
2021-07-04 10:57:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layoutsass2facebook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.layoutsass2facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
